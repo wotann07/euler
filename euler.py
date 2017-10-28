59eba3558c9f2151a398bc25a1d3b7e39d34351d
Abdel Gomez Perez <nabdel07@gmail.com>
2017-10-28 10:25:19 -0400

    Workspace update. Optimizations
    Optimized problem 2. Added command line input for problem number (interactive). Solved problem 20

--- euler.py
+++ euler.py
@@ -1,32 +1,63 @@
 import getopt
 import math
 import sys
+from decimal import Decimal
+from math import log
+
+__long_ass_digit = '73167176531330624919225119674426574742355349194934'
 
 
-__long_ass_digit = '73167176531330624919225119674426574742355349194934\
-96983520312774506326239578318016984801869478851843\
-85861560789112949495459501737958331952853208805511\
-12540698747158523863050715693290963295227443043557\
-66896648950445244523161731856403098711121722383113\
-62229893423380308135336276614282806444486645238749\
-30358907296290491560440772390713810515859307960866\
-70172427121883998797908792274921901699720888093776\
-65727333001053367881220235421809751254540594752243\
-52584907711670556013604839586446706324415722155397\
-53697817977846174064955149290862569321978468622482\
-83972241375657056057490261407972968652414535100474\
-82166370484403199890008895243450658541227588666881\
-16427171479924442928230863465674813919123162824586\
-17866458359124566529476545682848912883142607690042\
-24219022671055626321111109370544217506941658960408\
-07198403850962455444362981230987879927244284909188\
-84580156166097919133875499200524063689912560717606\
-05886116467109405077541002256983155200055935729725\
-71636269561882670428252483600823257530420752963450'
+def summatorial(n):
+    return n * (n + 1) / 2
+
+
+def pascal_row(n):
+    row = [1]
+    for k in range(1, n):
+        row.append(row[k - 1] * (n + 1 - k) / k)
+    if n is not 1:
+        row.append(1)
+    return row
+
+
+def is_whole_integer(c):
+    return c.real > 0 and Decimal(c.real) % 1 == 0 and c.imag == 0
+
+
+def solve_quadratic(a, b, c):
+    x = (-b + (b ** 2 - 4 * a * c) ** (1 / 2.0)) / 2
+    y = (-b - (b ** 2 - 4 * a * c) ** (1 / 2.0)) / 2
+    return x, y
+
+
+def max_triangle(file_name):
+    data = []
+    with open(file_name) as f:
+        for l in f.readlines():
+            int_line = []
+            for i in l.split():
+                int_line.append(int(i))
+            data.append(int_line)
+
+    for i in range(len(data) - 2, -1, -1):
+        for j in range(len(data[i])):
+            data[i][j] = data[i][j] + \
+                         max(data[i + 1][j], data[i + 1][j + 1])
+
+    return data[0][0]
 
 
 def collatz_sequence(n):
-    return
+    # The Collazt sequence is defined by n % 2 -> n / 2 else 3 * n + 1
+    seq = [n]
+    while n != 1:
+        if not n % 2:
+            n /= 2
+        else:
+            n = 3 * n + 1
+        seq.append(n)
+
+    return seq
 
 
 def triangle_number(n):
@@ -92,10 +123,10 @@ def sum_square_difference(n=100):
     sq_sum = 0
     cum_sum = 0
     for i in range(1, n + 1):
-        sq_sum += i**2
+        sq_sum += i ** 2
         cum_sum += i
 
-    return cum_sum**2 - sq_sum
+    return cum_sum ** 2 - sq_sum
 
 
 def is_palindrome(n):
@@ -145,18 +176,14 @@ def fibonacci(n):
 
 
 def fibo_sum_even(upper=4000000):
+    x = y = 1
     n = 0
-    i = 2
-    total = 0
-    while True:
-        n = fibonacci(i)
-        if n < upper:
-            i += 3
-            total += n
-        else:
-            break
+    while n < upper:
+        n += (x + y)
+        x, y = x + 2 * y, 2 * x + 3 * y
+        print (x, y)
 
-    return total
+    return n
 
 
 def three_five_multiples(below=1000):
@@ -198,7 +225,7 @@ def process(arg):
             i += 2
 
         print count
-    elif arg == '11':
+    elif arg == '12':
         i = 10
         while True:
             value = triangle_number(i)
@@ -208,14 +235,75 @@ def process(arg):
                 break
             i += 1
     elif arg == '13':
-        with open('p13.txt', 'r') as f:
+        with open('p13.txt') as f:
             addition = 0
             for l in f.readlines():
                 addition += int(l)
 
             print str(addition)[:10]
     elif arg == '14':
-        pass
+        longest = 0
+        starter = 0
+        for i in range(1, 1000000):
+            seq = collatz_sequence(i)
+            (longest, starter) = (longest, starter) if len(
+                seq) < longest else (len(seq), i)
+        print (longest, starter)
+    elif arg == '15':
+        addition = 0
+        power = str(2 ** 1000)
+        print power
+        for c in power:
+            addition += int(c)
+        print addition
+    elif arg == '18':
+        print max_triangle('p18.txt')
+    elif arg == '20':
+        n = math.factorial(100)
+        count = 0
+        for c in str(n):
+            count += int(c)
+        print count
+    elif arg == '67':
+        print max_triangle('p67.txt')
+    elif arg == '99':
+        data = []
+        with open('p99.txt') as f:
+            data = map(lambda s: (int(s[0]), int(s[1])), map(lambda line: line.split(','), f.readlines()))
+        print max(range(len(data)), key=lambda p: data[p][1] * log(data[p][0])) + 1
+    elif arg == '100':
+        n = 10 ** 12 - 1
+        while True:
+            (x, y) = solve_quadratic(1, -1, -(n * (n - 1) / 2))
+            print (x.real, n, y.real, x, y)
+            if is_whole_integer(x):
+                break
+            elif is_whole_integer(y):
+                break
+            n = n + 1
+
+        print (x, y)
+    elif arg == '148':
+        c = 0
+        print pascal_row(2)
+        for i in range(1, 21):
+            row = pascal_row(i)
+            print row
+            for j in row:
+                if not 0 == j % 7:
+                    c += 1
+        print c
+        # total of numbers in 10^9
+        n = 14
+        entities = summatorial(n)
+        print entities
+        # rows that are divisible are the sum of the entities on rows multiple of 7 minus 2 * number of rows
+        divisible_number_of_rows = (n - n % 7) / 7
+        print divisible_number_of_rows
+        divisible_entities = summatorial(
+            divisible_number_of_rows) * 7 - divisible_number_of_rows  # accounts for the extra 1
+        undivisible = entities - divisible_entities
+        print undivisible
     else:
         raise ValueError('Not Implemented')
 
@@ -228,6 +316,7 @@ def main():
         print msg
         print 'for help use --help'
         sys.exit(2)
+
     # process options
     for o in opts:
         if o in ('-h', '--help'):
@@ -235,8 +324,9 @@ def main():
             sys.exit(0)
 
     # process arguments
-    for arg in args:
-        process(arg)
+    while True:
+        euler = raw_input("Please enter problem number: ")
+        process(euler.strip())
 
 
 if __name__ == '__main__':
